4ea58b8eaf433e47e7babc3171826b854df9fef7
Ado Pabianko <ado.pabianko@efishery.com>
2022-01-05 22:52:05 +0700

    model serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
 from api.models import Person, Course, Grade
 
-class PersonSerializer(serializers.HyperlinkedModelSerializer):
+class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = ('first_name', 'last_name', 'courses')
 
-class CourseSerializer(serializers.HyperlinkedModelSerializer):
+class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ('name', 'year')
 
-class GradeSerializer(serializers.HyperlinkedModelSerializer):
+class GradeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Grade
         fields = ('person', 'course', 'grade')
